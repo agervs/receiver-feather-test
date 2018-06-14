0184f670ea8b9066ef2205af00b3ba1a486a4b4f
Alex <agervs@gmail.com>
2018-06-14 15:29:54 -0400

    Now recieves either o, w, l, or b from nano and prints out lunch detected, book detected, or water detected.

--- receiver-feather-test.ino
+++ receiver-feather-test.ino
@@ -12,18 +12,37 @@
 
 #include <Wire.h>
 
+int lunchDetected = 0;
+int bookDetected = 0;
+int waterDetected = 0;
+int numberOfTimesLunchDetected = 0;
+int numberOfTimesBookDetected = 0;
+int numberOfTimesWaterDetected = 0;
+int lunchNotDetectedCount = 0;
+int bookNotDetectedCount = 0;
+int waterNotDetectedCount = 0;
+int lunchDetectedThroughThisLoop;
+int bookDetectedThroughThisLoop;
+int waterDetectedThroughThisLoop;
+
 void setup() {
   Wire.begin();        // join i2c bus (address optional for master)
-  Serial.begin(9600);  // start serial for output
+  Serial.begin(115200);  // start serial for output
 }
 
 void loop() {
-  Wire.requestFrom(8, 6);    // request 6 bytes from slave device #8
+  Wire.requestFrom(8, 1);    // request 6 bytes from slave device #8
 
   while (Wire.available()) { // slave may send less than requested
     char c = Wire.read(); // receive a byte as character
-    Serial.print(c);         // print the character
+//    Serial.print(c);         // print the character
+    if (c == 'l') {
+      Serial.println("lunch detected");
+    } else if (c == 'b') {
+      Serial.println("book detected");  
+    } else if (c == 'w') {
+      Serial.println("water detected");  
+    }
   }
-
-  delay(500);
+//  Serial.println();
 }
